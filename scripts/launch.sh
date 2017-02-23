94f230ff3e8c94c5b9ce70e5523db01a90520c2a
Evan Rusin <evan.rusin@outlook.com>
2017-02-23 15:43:56 +0200

    devops

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,6 +1,5 @@
-docker-compose stop
-docker kill $(docker ps -q) || true
 docker rm $(docker ps -a -q) || true
 docker rmi $(docker images -q -f dangling=true) || true
+docker-compose stop
 docker-compose build 
 docker-compose up -d
